8ffadf2b8878e5e8f196f703d3d9983dc546459f
Abhinav <abhinavshaji16@gmail.com>
2023-12-14 16:42:36 +0530

    ADD Post, PostList, CreatePost templates

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
+import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Sidebar />
       <div className="content">
         <Header />
+        <CreatePost />
+        <PostList />
         <Footer />;
       </div>
     </div>
