bc8ac91b3f108281ded4c0e3c659c80e908532c3
Abhinav <abhinavshaji16@gmail.com>
2023-12-15 21:01:31 +0530

    Create a default Post data and pass it in the hook reducer and display Posts dynamically

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,17 +1,24 @@
 import { createContext, useReducer } from "react";
 
-const PostList = createContext({
+export const PostList = createContext({
   postList: [],
   addPost: () => {},
   deletePost: () => {},
 });
+
+const postListReducer = (currPostList, action) => {
+  return currPostList;
+};
 
 const PostListProvider = ({ children }) => {
   const addPost = () => {};
 
   const deletePost = () => {};
 
-  const [postList, dispatchPostList] = useReducer();
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
 
   return (
     <PostList.Provider value={{ postList, addPost, deletePost }}>
@@ -19,5 +26,24 @@ const PostListProvider = ({ children }) => {
     </PostList.Provider>
   );
 };
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going To Kerala",
+    body: "Hi Friends I am going to kerala for my vacations. Hope to enjoy.Peace out....",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "Kerala", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Passed",
+    body: "Passed my degree in BCA with good scores<.>",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Graduating", "Unbelivable"],
+  },
+];
 
 export default PostListProvider;
