65275e513c2a055a8f7058e07be9a71f5e9a5c50
Abhinav <abhinavshaji16@gmail.com>
2023-12-15 15:57:53 +0530

    render Home And CreatePost based on usestate value

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-const Sidebar = () => {
+const Sidebar = ({ selectedTab }) => {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
@@ -15,8 +15,8 @@ const Sidebar = () => {
       </a>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item">
-          <a href="#" className="nav-link active" aria-current="page">
+        <li className="nav-item ">
+          <a href="#" className={`nav-link text-white ${selectedTab ==="Home" && "active"}`} aria-current="page">
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#home"></use>
             </svg>
@@ -24,7 +24,7 @@ const Sidebar = () => {
           </a>
         </li>
         <li>
-          <a href="#" className="nav-link text-white">
+          <a href="#" className={`nav-link text-white ${selectedTab ==="Create Post" && "active"}`}>
             <svg className="bi pe-none me-2" width="16" height="16">
               <use xlinkHref="#speedometer2"></use>
             </svg>
