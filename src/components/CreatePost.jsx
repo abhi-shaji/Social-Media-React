0e632636be928d59d7c82775644adcce0b9588c9
Abhinav <abhinavshaji16@gmail.com>
2023-12-16 10:45:59 +0530

    setup create post UI version one with useRef for each input element

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,42 +1,82 @@
+import { useContext, useRef } from "react";
+import { PostList } from "../store/post-list-store";
+
 const CreatePost = () => {
+  const { addPost } = useContext(PostList);
+
+  const userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const reactionsElement = useRef();
+  const tagsElement = useRef();
+
+  
+
   return (
-    <form>
+    <form className="create-post" onSubmit={handleSubmit}>
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+        <label htmlFor="userId" className="form-label">
+          Enter your UserId here
         </label>
         <input
-          type="email"
+          type="text"
           className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
+          id="userId"
+          placeholder="Your User Id"
+          ref={userIdElement}
         />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
+        <label htmlFor="title" className="form-label">
+          Post Title
         </label>
         <input
-          type="password"
+          type="text"
           className="form-control"
-          id="exampleInputPassword1"
+          id="title"
+          placeholder="How are you feeling today"
+          ref={postTitleElement}
         />
       </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Check me out
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
         </label>
+        <textarea
+          type="text"
+          className="form-control"
+          id="body"
+          placeholder="Tell us more about  it"
+          ref={postBodyElement}
+        />
       </div>
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of reactions
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="reactions"
+          placeholder="How many people reacted to this post"
+          ref={reactionsElement}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter your Hashtags here
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="tags"
+          placeholder="Please enter tags using space"
+          ref={tagsElement}
+        />
+      </div>
+
       <button type="submit" className="btn btn-primary">
-        Submit
+        Post
       </button>
     </form>
   );
